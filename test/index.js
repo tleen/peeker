40a36465e3aa74bafe34dfd2cb058c00f57d1692
Thomas Leen <tl@thomasleen.com>
2015-02-06 12:52:11 -0500

    initial project framework

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* global describe, it, should */
+
+describe('boilerplate test setup', function(){
+  it('should return true', function(){
+    (1 + 1).should.equal(2);
+  });
+});
+
